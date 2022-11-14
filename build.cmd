1f8f614dbc2b6b56716cab6a4792285d636230ce
Juris Liepins <juris.liepins@hotmail.com>
2022-11-14 20:31:59 +0000

    Added FBitTorrent.Core

--- build.cmd
+++ build.cmd
@@ -1,3 +1,8 @@
 @echo off
 dotnet tool restore
 dotnet paket restore
+
+@REM TODO: Use FAKE build.fsx once path too long issue has been fixes.
+dotnet restore
+dotnet clean
+dotnet build
--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 dotnet tool restore
 dotnet paket restore
+
+# TODO: Use FAKE build.fsx once path too long issue has been fixes.
+dotnet restore
+dotnet clean
+dotnet build
