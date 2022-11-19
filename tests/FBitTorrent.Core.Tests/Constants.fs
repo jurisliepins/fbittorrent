59093eed48c0ccdd02d8399e151854f4bad763c5
Juris Liepins <juris.liepins@hotmail.com>
2022-11-19 22:16:02 +0000

    Added FileSystem module and tests

---
+++ tests/FBitTorrent.Core.Tests/Constants.fs
@@ -0,0 +1,19 @@
+﻿namespace FBitTorrent.Core.Tests
+
+open System.IO
+
+module Constants =
+    let announce = "https://torrent.ubuntu.com/announce"
+    
+    let singleFileMetaInfoPath = ".\\single_file.torrent"
+    let multiFileMetaInfoPath = ".\\multi_file.torrent"
+    
+    let singleFileTorrentPath = ".\\war_and_peace.txt"
+    let multiFileTorrentPath1 = ".\\war_and_peace\\file_1.txt"
+    let multiFileTorrentPath2 = ".\\war_and_peace\\file_2.txt"
+    let multiFileTorrentPath3 = ".\\war_and_peace\\file_3.txt"
+    let multiFileTorrentPath4 = ".\\war_and_peace\\file_4.txt"
+    
+    let singleFileMetaInfoBytes = File.ReadAllBytes(singleFileMetaInfoPath)
+    let multiFileMetaInfoBytes  = File.ReadAllBytes(multiFileMetaInfoPath)
+    
---
+++ tests/FBitTorrent.Core.Tests/FileSystemTests.fs
@@ -0,0 +1,59 @@
+﻿namespace FBitTorrent.Core.Tests
+
+open System
+open System.IO
+open FBitTorrent.Core
+open Xunit
+
+module FileSystemTests =
+    
+    let validFilePath = ".\\test.txt"
+    let validDirPath = ".\\test"
+    
+    let invalidFilePath = ""
+    let invalidDirPath = ""
+    
+    let local = FileSystem.createLocal ()
+    
+    [<Fact>]
+    let ``Test local file exists`` () =
+        let stream = File.Create validFilePath
+        stream.Close()
+        Assert.True(local.FileExists validFilePath)
+        File.Delete validFilePath
+        
+    [<Fact>]
+    let ``Test local file doesn't exists`` () =
+        Assert.False(local.FileExists validFilePath)
+        File.Delete validFilePath
+        
+    [<Fact>]
+    let ``Test local dir exists`` () =
+        let _ = Directory.CreateDirectory validDirPath
+        Assert.True(local.DirectoryExists validDirPath)
+        Directory.Delete validDirPath
+        
+    [<Fact>]
+    let ``Test local dir doesn't exists`` () =
+        Assert.False(local.DirectoryExists validDirPath)
+    
+    [<Fact>]
+    let ``Test local open or create success`` () =
+        let stream = File.Create validFilePath
+        stream.Close()
+        let stream = local.OpenOrCreate validFilePath
+        stream.Close()
+        File.Delete validFilePath
+        
+    [<Fact>]
+    let ``Test local file open or create failure`` () =
+        Assert.ThrowsAny<Exception>(fun () -> local.OpenOrCreate invalidFilePath |> ignore)
+        
+    [<Fact>]
+    let ``Test local create dir success`` () =
+        let _ = local.CreateDirectory validDirPath
+        Directory.Delete validDirPath
+        
+    [<Fact>]
+    let ``Test local create dir failure`` () =
+        Assert.ThrowsAny<Exception>(fun () -> local.CreateDirectory invalidDirPath)
