37face4e413be1f916ef4e6c72c027741cdf4cb0
Juris Liepins <juris.liepins@hotmail.com>
2022-11-20 15:28:40 +0000

    Added Rate module and tests

---
+++ src/FBitTorrent.Core/Rate.fs
@@ -0,0 +1,53 @@
+﻿namespace FBitTorrent.Core
+
+module Rate =
+    let [<Literal>] Smoothing = 0.02
+    let [<Literal>] Capacity = 5
+
+type Rate =
+    val mutable private speeds: int64[]
+    val mutable private byteCount: int64
+    val mutable private pointer: int
+    val mutable private length: int
+    new(initialByteCount: int64) = {
+        speeds = Array.create Rate.Capacity 0L
+        byteCount = initialByteCount
+        pointer = 0
+        length = 0 }
+    
+    member __.Update(lastBytes: int64, per: int) =
+        let idx = (__.pointer + __.length) % __.speeds.Length
+        __.speeds[idx] <- (lastBytes - __.byteCount) / int64 per
+        __.byteCount <- lastBytes
+        if __.length >= __.speeds.Length then
+            __.pointer <- __.pointer + 1
+        else
+            __.length <- __.length + 1
+
+    member __.GetSpeed() =
+        if __.length < 1 then
+            double 0.0
+        else
+            let last = double (__.speeds[((__.pointer + __.length - 1) % __.speeds.Length)])
+            let total = double (Array.sum __.speeds[0..(__.length - 1)])
+            let count = double __.length
+            let average = total / count
+            // Exponential moving average to smooth out the result.
+            double ((Rate.Smoothing * last) + ((1.0 - Rate.Smoothing) * average))
+
+    override __.ToString() =
+        let speed = __.GetSpeed()
+        if   speed < (1024.0)                            then $"%.3f{(speed)} B/s"
+        elif speed < (1024.0 * 1024.0)                   then $"%.3f{(speed / 1024.0)} KB/s"
+        elif speed < (1024.0 * 1024.0 * 1024.0)          then $"%.3f{(speed / 1024.0 / 1024.0)} MB/s"
+        elif speed < (1024.0 * 1024.0 * 1024.0 * 1024.0) then $"%.3f{(speed / 1024.0 / 1024.0 / 1024.0)} GB/s"
+        else
+            failwith $"Invalid speed value %.3f{speed}"
+    
+    override __.GetHashCode() = __.GetSpeed().GetHashCode()
+    
+    override __.Equals(other: obj) =
+        match other with
+        | :? Rate as other ->
+            __.GetSpeed() = other.GetSpeed()
+        | _ -> false        
---
+++ tests/FBitTorrent.Core.Tests/RateTests.fs
@@ -0,0 +1,58 @@
+﻿namespace FBitTorrent.Core.Tests
+
+open FBitTorrent.Core
+open Xunit
+
+module RateTests =
+    
+    [<Fact>]
+    let ``Test rate should measure`` () =
+        let rate = Rate 0
+        rate.Update(1000, 1)
+        Assert.Equal(1000.0, rate.GetSpeed())
+        rate.Update(2500, 1)
+        Assert.Equal(1255.0, rate.GetSpeed())
+        rate.Update(3500, 1)
+        Assert.Equal(1163.3333333333335, rate.GetSpeed())
+        rate.Update(4750, 1)
+        Assert.Equal(1188.75, rate.GetSpeed())
+        rate.Update(5000, 1)
+        Assert.Equal(985.0, rate.GetSpeed())
+        rate.Update(5000, 1)
+        Assert.Equal(784.0, rate.GetSpeed())
+        rate.Update(5000, 1)
+        Assert.Equal(490.0, rate.GetSpeed())
+        rate.Update(5125, 1)
+        Assert.Equal(321.0, rate.GetSpeed())
+        rate.Update(5145, 1)
+        Assert.Equal(77.82000000000001, rate.GetSpeed())
+        rate.Update(5225, 1)
+        Assert.Equal(45.7000, rate.GetSpeed())
+        
+    [<Fact>]
+    let ``Test rate should format bytes`` () =
+        let exp = "10.000 B/s"
+        let act = Rate(0L)
+        act.Update(10L, 1)                      
+        Assert.Equal(exp, act.ToString())
+        
+    [<Fact>]
+    let ``Test rate should format kilo-bytes`` () =
+        let exp = "10.000 KB/s"
+        let act = Rate(0L)
+        act.Update(10L * 1024L, 1)
+        Assert.Equal(exp, act.ToString())
+        
+    [<Fact>]
+    let ``Test rate should format mega-bytes`` () =
+        let exp = "10.000 MB/s"
+        let act = Rate(0L)
+        act.Update(10L * 1024L * 1024L, 1)
+        Assert.Equal(exp, act.ToString())
+        
+    [<Fact>]
+    let ``Test rate should format giga-bytes`` () =
+        let exp = "10.000 GB/s"
+        let act = Rate(0L)
+        act.Update(10L * 1024L * 1024L * 1024L, 1)
+        Assert.Equal(exp, act.ToString())
