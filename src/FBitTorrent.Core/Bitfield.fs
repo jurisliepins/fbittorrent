6bbfb594ff9d1815f06a4f6d562c4b344ddfa817
Juris Liepins <juris.liepins@hotmail.com>
2022-11-20 14:24:37 +0000

    Added Bitfield and tests

---
+++ src/FBitTorrent.Core/Bitfield.fs
@@ -0,0 +1,89 @@
+﻿namespace FBitTorrent.Core
+
+open System
+open System.Linq
+
+type Bitfield =
+    val mutable private bytes: byte[]
+    val mutable private capacity: int
+    val mutable private count: int
+    new(capacity: int) = {
+        bytes =
+            if (capacity % 8) <> 0 then
+                (Array.create ((capacity / 8) + 1) 0uy)
+            else
+                (Array.create (capacity / 8) 0uy)
+        capacity = capacity
+        count = 0 }
+    new(bitfield: Bitfield) = {
+        bytes = bitfield.ToArray()
+        capacity = bitfield.Capacity
+        count = bitfield.Count }
+
+    member __.Capacity with get() = __.capacity
+    
+    member __.Count with get() = __.count
+        
+    member __.IsEmpty with get() = __.Count = 0
+    
+    member __.IsFull with get() = __.Count = __.Capacity
+    
+    member __.Get(idx: int) =
+        if idx < 0 || idx >= __.Capacity then
+            false
+        else
+            let index = idx / 8
+            let offset = idx % 8
+            ((__.bytes[index] >>> (7 - offset)) &&& 1uy) <> 0uy
+    
+    member __.Set(idx: int, value: bool) =
+        if idx < 0 || idx >= __.Capacity then
+            ()
+        else
+            let index = idx / 8
+            let offset = idx % 8
+            if value then
+                if not (__.Get(idx)) then
+                    __.bytes[index] <- __.bytes[index] ||| (1uy <<< (7 - offset))
+                    __.count <- __.count + 1
+            else
+                if __.Get(idx) then
+                    __.bytes[index] <- __.bytes[index] &&& ~~~(1uy <<< (7 - offset))
+                    __.count <- __.count - 1
+    
+    member __.Set(bytes: byte[]) =
+        __.count <- 0
+        for idx in 0..__.bytes.Length - 1 do
+            if idx < bytes.Length then
+                __.bytes[idx] <- bytes[idx]
+                for offset in 0..7 do
+                    if __.count < __.capacity then
+                        __.count <- __.count + int ((__.bytes[idx] >>> (7 - offset)) &&& 1uy)
+            else
+                __.bytes[idx] <- 0uy
+    
+    member __.And(bitfield: Bitfield) = __
+    
+    member __.Or(bitfield: Bitfield) = __
+    
+    member __.Xor(bitfield: Bitfield) = __
+    
+    member __.Not() =
+        for idx in 0..__.bytes.Length - 1 do
+            __.bytes[idx] <- ~~~__.bytes[idx] 
+        __.count <- __.capacity - __.count
+        __
+    
+    member __.ToArray() = __.bytes.ToArray()
+    
+    override __.ToString() = String.Join("\n", [for byte in __.bytes do Convert.ToString(byte, 2).PadLeft(8, '0')])
+    
+    override __.GetHashCode() = __.bytes.Aggregate(0, (fun result byte -> (result * 31) ^^^ (int byte)))
+    
+    override __.Equals(other) =
+        match other with
+        | :? Bitfield as value ->
+            Enumerable.SequenceEqual(__.bytes, value.ToArray())
+        | _ -> false
+    
+    
---
+++ tests/FBitTorrent.Core.Tests/BitfieldTests.fs
@@ -0,0 +1,184 @@
+﻿namespace FBitTorrent.Core.Tests
+
+open FBitTorrent.Core
+open Xunit
+
+module BitfieldTests =
+    [<Fact>]
+    let ``Test bitfield should get value on`` () =
+        let bitfield = Bitfield(4 * 8)
+        bitfield.Set(([| 1uy; 2uy; 3uy; 4uy |]))
+        Assert.True(bitfield.Get(7))
+        Assert.True(bitfield.Get(14))
+        Assert.True(bitfield.Get(22))
+        Assert.True(bitfield.Get(23))
+        Assert.True(bitfield.Get(29))
+        Assert.False(bitfield.Get(0))
+        Assert.False(bitfield.Get(1))
+        Assert.False(bitfield.Get(2))
+        Assert.False(bitfield.Get(3))
+        Assert.False(bitfield.Get(4))
+        Assert.False(bitfield.Get(5))
+        Assert.False(bitfield.Get(6))
+        
+    [<Fact>]
+    let ``Test bitfield should get value off`` () =
+        let bitfield = Bitfield(4 * 8)
+        bitfield.Set([| 1uy; 2uy; 3uy; 4uy |])
+        Assert.False(not (bitfield.Get(7)))
+        Assert.False(not (bitfield.Get(14)))
+        Assert.False(not (bitfield.Get(22)))
+        Assert.False(not (bitfield.Get(23)))
+        Assert.False(not (bitfield.Get(29)))
+        Assert.True(not (bitfield.Get(0)))
+        Assert.True(not (bitfield.Get(1)))
+        Assert.True(not (bitfield.Get(2)))
+        Assert.True(not (bitfield.Get(3)))
+        Assert.True(not (bitfield.Get(4)))
+        Assert.True(not (bitfield.Get(5)))
+        Assert.True(not (bitfield.Get(6)))
+    
+    [<Fact>]
+    let ``Test bitfield should set value on`` () =
+        let bitfield = Bitfield(4 * 8)
+        bitfield.Set([| 1uy; 2uy; 3uy; 4uy |])
+        bitfield.Set(0, true)
+        bitfield.Set(1, true)
+        bitfield.Set(2, true)
+        bitfield.Set(3, true)
+        bitfield.Set(4, true)
+        bitfield.Set(5, true)
+        bitfield.Set(6, true)
+        Assert.True(bitfield.Get(0))
+        Assert.True(bitfield.Get(1))
+        Assert.True(bitfield.Get(2))
+        Assert.True(bitfield.Get(3))
+        Assert.True(bitfield.Get(4))
+        Assert.True(bitfield.Get(5))
+        Assert.True(bitfield.Get(6))
+        
+    [<Fact>]
+    let ``Test bitfield should set value off`` () =
+        let bitfield = Bitfield(4 * 8)
+        bitfield.Set([| 1uy; 2uy; 3uy; 4uy |])
+        bitfield.Set(7, false)
+        bitfield.Set(14, false)
+        bitfield.Set(22, false)
+        bitfield.Set(23, false)
+        bitfield.Set(29, false)
+        Assert.True(not (bitfield.Get(7)))
+        Assert.True(not (bitfield.Get(14)))
+        Assert.True(not (bitfield.Get(22)))
+        Assert.True(not (bitfield.Get(23)))
+        Assert.True(not (bitfield.Get(29)))
+        
+    
+    [<Fact>]
+    let ``Test bitfield should set values`` () =
+        let bitfield = Bitfield(4 * 8)
+        for idx in 0..(4 * 8) - 1 do
+            Assert.True(not (bitfield.Get(idx)))
+        bitfield.Set([| 1uy; 2uy; 3uy; 4uy |])
+        Assert.True(bitfield.Get(7))
+        Assert.True(bitfield.Get(14))
+        Assert.True(bitfield.Get(22))
+        Assert.True(bitfield.Get(23))
+        Assert.True(bitfield.Get(29))
+        Assert.False(bitfield.Get(0))
+        Assert.False(bitfield.Get(1))
+        Assert.False(bitfield.Get(2))
+        Assert.False(bitfield.Get(3))
+        Assert.False(bitfield.Get(4))
+        Assert.False(bitfield.Get(5))
+        Assert.False(bitfield.Get(6))
+        bitfield.Set([| 255uy; 255uy; 255uy; 255uy
+                        255uy; 255uy; 255uy; 255uy; |])
+        Assert.Equal<byte[]>([| 255uy; 255uy; 255uy; 255uy |], bitfield.ToArray())
+    
+    [<Fact>]
+    let ``Test bitfield should and`` () =
+        ()
+        
+    [<Fact>]
+    let ``Test bitfield should or`` () =
+        ()
+        
+    [<Fact>]
+    let ``Test bitfield should xor`` () =
+        ()
+        
+    [<Fact>]
+    let ``Test bitfield should not`` () =
+        let bitfield = Bitfield(4 * 8)
+        for idx in 0..(4 * 8) - 1 do
+            Assert.True(not (bitfield.Get(idx)))
+        bitfield.Set([| 1uy; 2uy; 3uy; 4uy |])
+        bitfield.Not() |> ignore
+        Assert.True(not (bitfield.Get(7)))
+        Assert.True(not (bitfield.Get(14)))
+        Assert.True(not (bitfield.Get(22)))
+        Assert.True(not (bitfield.Get(23)))
+        Assert.True(not (bitfield.Get(29)))
+        Assert.False(not (bitfield.Get(0)))
+        Assert.False(not (bitfield.Get(1)))
+        Assert.False(not (bitfield.Get(2)))
+        Assert.False(not (bitfield.Get(3)))
+        Assert.False(not (bitfield.Get(4)))
+        Assert.False(not (bitfield.Get(5)))
+        Assert.False(not (bitfield.Get(6)))
+    
+    [<Fact>]
+    let ``Test bitfield should get count`` () =
+        let bitfield = Bitfield(4 * 8)
+        bitfield.Set(0, true)
+        Assert.Equal(1, bitfield.Count)
+        bitfield.Set(1, true)
+        Assert.Equal(2, bitfield.Count)
+        bitfield.Set(0, true)
+        Assert.Equal(2, bitfield.Count)
+        bitfield.Set(1, true)
+        Assert.Equal(2, bitfield.Count)
+        bitfield.Set(2, false)
+        Assert.Equal(2, bitfield.Count)
+        bitfield.Set(3, false)
+        Assert.Equal(2, bitfield.Count)
+        bitfield.Set(0, false)
+        Assert.Equal(1, bitfield.Count)
+        bitfield.Set(1, false)
+        Assert.Equal(0, bitfield.Count)
+        bitfield.Set([| 255uy; 255uy; 255uy; 255uy
+                        255uy; 255uy; 255uy; 255uy
+                        255uy; 255uy; 255uy; 255uy
+                        255uy; 255uy; 255uy; 255uy; |])
+        Assert.Equal(4 * 8, bitfield.Count)
+        
+    [<Fact>]
+    let ``Test bitfield should get capacity`` () =
+        let bitfield = Bitfield(31)
+        Assert.Equal(31, bitfield.Capacity)
+        let bitfield = Bitfield(32)
+        Assert.Equal(32, bitfield.Capacity)
+        let bitfield = Bitfield(33)
+        Assert.Equal(33, bitfield.Capacity)
+        let bitfield = Bitfield(34)
+        Assert.Equal(34, bitfield.Capacity)
+        
+    [<Fact>]
+    let ``Test bitfield should check is empty`` () =
+        let bitfield = Bitfield(4 * 8)
+        Assert.True(bitfield.IsEmpty)
+        bitfield.Set(0, true)
+        bitfield.Set(1, true)
+        bitfield.Set(2, true)
+        bitfield.Set(3, true)
+        Assert.False(bitfield.IsEmpty)
+        
+    [<Fact>]
+    let ``Test bitfield should check is full`` () =
+        let bitfield = Bitfield(4)
+        Assert.False(bitfield.IsFull)
+        bitfield.Set(0, true)
+        bitfield.Set(1, true)
+        bitfield.Set(2, true)
+        bitfield.Set(3, true)
+        Assert.True(bitfield.IsFull)
