954edebd071031ec35cff352c176d9fdfa61b5d6
Juris Liepins <juris.liepins@hotmail.com>
2022-11-26 12:30:50 +0000

    Minor Rate changes

--- src/FBitTorrent.Core/Rate.fs
+++ src/FBitTorrent.Core/Rate.fs
@@ -9,33 +9,28 @@ module Rate =
     let GB = 1024.0 * 1024.0 * 1024.0
     let TB = 1024.0 * 1024.0 * 1024.0 * 1024.0
 
-type Rate =
-    val mutable private speeds: int64[]
-    val mutable private byteCount: int64
-    val mutable private pointer: int
-    val mutable private length: int
-    new(initialByteCount: int64) = {
-        speeds = Array.create Rate.Capacity 0L
-        byteCount = initialByteCount
-        pointer = 0
-        length = 0 }
+type Rate() =
+    member val private Speeds = Array.create Rate.Capacity 0L with get 
+    member val private ByteCount = 0L with get, set
+    member val private Head = 0 with get, set
+    member val private Length = 0 with get, set
     
-    member __.Update(lastBytes: int64) =
-        let idx = (__.pointer + __.length) % __.speeds.Length
-        __.speeds[idx] <- (lastBytes - __.byteCount)
-        __.byteCount <- lastBytes
-        if __.length >= __.speeds.Length then
-            __.pointer <- __.pointer + 1
+    member __.Update(byteCount: int64) =
+        let idx = (__.Head + __.Length) % __.Speeds.Length
+        __.Speeds[idx] <- (byteCount - __.ByteCount)
+        __.ByteCount <- byteCount
+        if __.Length >= __.Speeds.Length then
+            __.Head <- __.Head + 1
         else
-            __.length <- __.length + 1
+            __.Length <- __.Length + 1
 
     member __.GetSpeed() =
-        if __.length < 1 then
+        if __.Length < 1 then
             double 0.0
         else
-            let last = double (__.speeds[((__.pointer + __.length - 1) % __.speeds.Length)])
-            let total = double (Array.sum __.speeds[0..(__.length - 1)])
-            let count = double __.length
+            let last = double (__.Speeds[((__.Head + __.Length - 1) % __.Speeds.Length)])
+            let total = double (Array.sum __.Speeds[0..(__.Length - 1)])
+            let count = double __.Length
             let average = total / count
             // Exponential moving average to smooth out the result.
             double ((Rate.Smoothing * last) + ((1.0 - Rate.Smoothing) * average))
--- tests/FBitTorrent.Core.Tests/RateTests.fs
+++ tests/FBitTorrent.Core.Tests/RateTests.fs
@@ -7,7 +7,7 @@ module RateTests =
     
     [<Fact>]
     let ``Test rate should measure`` () =
-        let rate = Rate 0
+        let rate = Rate()
         rate.Update(1000)
         Assert.Equal(1000.0, rate.GetSpeed())
         rate.Update(2500)
@@ -32,27 +32,27 @@ module RateTests =
     [<Fact>]
     let ``Test rate should format bytes`` () =
         let exp = "10.000 B/s"
-        let act = Rate(0L)
+        let act = Rate()
         act.Update(10L)                      
         Assert.Equal(exp, act.ToString())
         
     [<Fact>]
     let ``Test rate should format kilo-bytes`` () =
         let exp = "10.000 KB/s"
-        let act = Rate(0L)
+        let act = Rate()
         act.Update(10L * 1024L)
         Assert.Equal(exp, act.ToString())
         
     [<Fact>]
     let ``Test rate should format mega-bytes`` () =
         let exp = "10.000 MB/s"
-        let act = Rate(0L)
+        let act = Rate()
         act.Update(10L * 1024L * 1024L)
         Assert.Equal(exp, act.ToString())
         
     [<Fact>]
     let ``Test rate should format giga-bytes`` () =
         let exp = "10.000 GB/s"
-        let act = Rate(0L)
+        let act = Rate()
         act.Update(10L * 1024L * 1024L * 1024L)
         Assert.Equal(exp, act.ToString())
