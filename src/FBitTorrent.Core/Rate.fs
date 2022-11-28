bab99806383b9a03ada80e243f6c56516caad8d8
Juris Liepins <juris.liepins@hotmail.com>
2022-11-28 22:48:51 +0000

    Implemented Rate with a circular buffer

--- src/FBitTorrent.Core/Rate.fs
+++ src/FBitTorrent.Core/Rate.fs
@@ -1,31 +1,74 @@
 ﻿namespace FBitTorrent.Core
 
+open System.Linq
+open System.Collections
+open System.Collections.Generic
+
+type CircularBuffer<'a>(capacity: int) =
+    let mutable values = Array.zeroCreate<'a> capacity
+    let mutable pointer = 0
+    let mutable count = 0
+    
+    member _.Values with get() = values
+    member _.Pointer with get() = pointer
+    member _.Count with get() = count
+    
+    member _.Push(value: 'a) =
+        if count >= values.Length then
+            values[(pointer + count) % values.Length] <- value
+            pointer <- pointer + 1
+        else
+            values[(pointer + count) % values.Length] <- value
+            count <- count + 1
+            
+    interface IEnumerable with
+        member __.GetEnumerator() = new CircularBufferEnumerator<'a>(__)
+        
+    interface IEnumerable<'a> with
+        member __.GetEnumerator() = new CircularBufferEnumerator<'a>(__)
+            
+and CircularBufferEnumerator<'a>(buffer: CircularBuffer<'a>) =
+    
+    member val private Current = Unchecked.defaultof<'a> with get, set
+    member val private Index = 0 with get, set
+    
+    interface IEnumerator<'a> with
+        member __.Current with get() = __.Current
+            
+        member _.Dispose() = ()
+    
+    interface IEnumerator with
+        member __.Current with get() = __.Current :> obj
+        
+        member __.MoveNext() =
+            if __.Index < buffer.Count then
+                __.Current <- buffer.Values[(buffer.Pointer + __.Index) % buffer.Values.Length]
+                __.Index <- __.Index + 1
+                true
+            else
+                false
+                
+        member __.Reset() = __.Index <- 0
+
 module Rate =
     let [<Literal>] Smoothing = 0.02
     let [<Literal>] Capacity = 5
 
 type Rate() =
-    member val private Speeds = Array.create Rate.Capacity 0L with get 
+    member val private ByteCounts = CircularBuffer(Rate.Capacity) with get
     member val private ByteCount = 0L with get, set
-    member val private Head = 0 with get, set
-    member val private Length = 0 with get, set
     
     member __.Update(byteCount: int64) =
-        let idx = (__.Head + __.Length) % __.Speeds.Length
-        __.Speeds[idx] <- (byteCount - __.ByteCount)
+        __.ByteCounts.Push(byteCount - __.ByteCount)
         __.ByteCount <- byteCount
-        if __.Length >= __.Speeds.Length then
-            __.Head <- __.Head + 1
-        else
-            __.Length <- __.Length + 1
 
     member __.GetSpeed() =
-        if __.Length < 1 then
+        if __.ByteCounts.Count < 1 then
             double 0.0
         else
-            let last = double (__.Speeds[((__.Head + __.Length - 1) % __.Speeds.Length)])
-            let total = double (Array.sum __.Speeds[0..(__.Length - 1)])
-            let count = double __.Length
+            let last = double (__.ByteCounts.Last())
+            let total = double (__.ByteCounts.Sum())
+            let count = double __.ByteCounts.Count
             let average = total / count
             // Exponential moving average to smooth out the result.
             double ((Rate.Smoothing * last) + ((1.0 - Rate.Smoothing) * average))
--- tests/FBitTorrent.Core.Tests/RateTests.fs
+++ tests/FBitTorrent.Core.Tests/RateTests.fs
@@ -1,9 +1,60 @@
 ﻿namespace FBitTorrent.Core.Tests
 
+open System.Linq
+open System.Collections.Generic
 open FBitTorrent.Core
 open Xunit
 
 module RateTests =
+    
+    [<Fact>]
+    let ``Test circular buffer should push and enumerate`` () =
+        let buffer = CircularBuffer<int>(5)
+        Assert.Equal(0L, buffer.LongCount())
+        let enumerator1 = (buffer :> IEnumerable<int>).GetEnumerator()
+        Assert.False(enumerator1.MoveNext())
+        buffer.Push(1)
+        Assert.Equal(1L, buffer.LongCount())
+        buffer.Push(2)
+        Assert.Equal(2L, buffer.LongCount())
+        buffer.Push(3)
+        Assert.Equal(3L, buffer.LongCount())
+        buffer.Push(4)
+        Assert.Equal(4L, buffer.LongCount())
+        buffer.Push(5)
+        Assert.Equal(5L, buffer.LongCount())
+        let enumerator2 = (buffer :> IEnumerable<int>).GetEnumerator()
+        Assert.True(enumerator2.MoveNext())
+        Assert.Equal(1, enumerator2.Current)
+        Assert.True(enumerator2.MoveNext())
+        Assert.Equal(2, enumerator2.Current)
+        Assert.True(enumerator2.MoveNext())
+        Assert.Equal(3, enumerator2.Current)
+        Assert.True(enumerator2.MoveNext())
+        Assert.Equal(4, enumerator2.Current)
+        Assert.True(enumerator2.MoveNext())
+        Assert.Equal(5, enumerator2.Current)
+        buffer.Push(6)
+        Assert.Equal(5L, buffer.LongCount())
+        buffer.Push(7)
+        Assert.Equal(5L, buffer.LongCount())
+        buffer.Push(8)
+        Assert.Equal(5L, buffer.LongCount())
+        buffer.Push(9)
+        Assert.Equal(5L, buffer.LongCount())
+        buffer.Push(10)
+        Assert.Equal(5L, buffer.LongCount())
+        let enumerator3 = (buffer :> IEnumerable<int>).GetEnumerator()
+        Assert.True(enumerator3.MoveNext())
+        Assert.Equal(6, enumerator3.Current)
+        Assert.True(enumerator3.MoveNext())
+        Assert.Equal(7, enumerator3.Current)
+        Assert.True(enumerator3.MoveNext())
+        Assert.Equal(8, enumerator3.Current)
+        Assert.True(enumerator3.MoveNext())
+        Assert.Equal(9, enumerator3.Current)
+        Assert.True(enumerator3.MoveNext())
+        Assert.Equal(10, enumerator3.Current)
     
     [<Fact>]
     let ``Test rate should measure`` () =
